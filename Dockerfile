e4366eb91402de56fc10ee3bb40415b44e206480
Kurimochi <kurimochi.sec@gmail.com>
2025-06-20 21:02:38 +0900

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM python:3.13.5-slim AS builder
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install --user -r requirements.txt
+
+FROM python:3.13.5-slim
+WORKDIR /app
+
+COPY --from=builder /root/.local /root/.local
+ENV PATH=/root/.local/bin:$PATH
+
+COPY ./src /app/
+
+CMD ["python3", "src/bot.py"]
---
+++ src/bot.py
@@ -0,0 +1,323 @@
+import discord
+import sqlite3
+import asyncio
+import re
+import os
+from discord import app_commands
+from discord.ext import tasks
+from datetime import datetime
+
+TOKEN = os.getenv('TOKEN')
+DB_FILE = os.getenv('DB_FILE')
+BACKUP_DB_FILE = os.getenv('BACKUP_DB_FILE')
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+
+# DB初期化
+conn = sqlite3.connect(DB_FILE)
+cursor = conn.cursor()
+backup_conn = sqlite3.connect(BACKUP_DB_FILE)
+backup_cursor = backup_conn.cursor()
+
+cursor.execute('CREATE TABLE IF NOT EXISTS progress ( \
+                    id INTEGER PRIMARY KEY, \
+                    user_id INTEGER NOT NULL, \
+                    message TEXT NOT NULL, \
+                    timestamp DEFAULT CURRENT_TIMESTAMP \
+                )')
+cursor.execute('CREATE TABLE IF NOT EXISTS users ( \
+                    id INTEGER PRIMARY KEY, \
+                    user_id INTEGER NOT NULL, \
+                    channel INTEGER NOT NULL, \
+                    notice TEXT \
+                )')
+cursor.execute('CREATE TABLE IF NOT EXISTS votes ( \
+                    id INTEGER PRIMARY KEY, \
+                    user_id INTEGER NOT NULL, \
+                    message_id INTEGER NOT NULL, \
+                    consent INTEGER NOT NULL, \
+                    refusal INTEGER NOT NULL \
+                )')
+
+backup_cursor.execute('CREATE TABLE IF NOT EXISTS progress ( \
+                            id INTEGER PRIMARY KEY, \
+                            user_id INTEGER NOT NULL, \
+                            message TEXT NOT NULL, \
+                            timestamp DEFAULT CURRENT_TIMESTAMP \
+                        )')
+
+conn.commit()
+backup_conn.commit()
+
+# ユーザーが登録しているかを判定
+def registered(user_id):
+    cursor.execute('SELECT user_id FROM users WHERE user_id = ?', (user_id,))
+    result = cursor.fetchone()
+
+    if result is None:
+        return False
+    else:
+        return True
+
+# channel設定が正当かを判定
+def channel_judge(channel):
+    if channel[:2] == '<#' and channel[-1] == '>':
+        channel = channel[2:-1]
+
+    if channel.isdigit() and client.get_channel(int(channel)) is not None:
+        return (channel, True)
+    else:
+        return (channel, False)
+
+# aggregate内のprogress取得
+def aggr_internal(user_id):
+    cursor.execute('SELECT message FROM progress WHERE user_id = ?', (user_id,))
+    progress = cursor.fetchall()
+    return progress
+
+# bot初期化
+@client.event
+async def on_ready():
+    print('Bot is ready.')
+    cron.start()
+    await tree.sync()
+
+# /register  prograssに登録
+@tree.command(name='register', description='prograssに登録')
+@app_commands.describe(channel='定期メッセージを送信するチャンネル (メンション or ID)')
+async def register(interaction: discord.Interaction, channel: str):
+    user_id = interaction.user.id
+
+    if registered(user_id):
+        embed = discord.Embed(
+            title='Error!',
+            color=0xbf1e33
+        )
+        embed.add_field(name='You are already registered.', value='')
+    else:
+        channel, judge = channel_judge(channel)
+        if judge:
+            cursor.execute('INSERT INTO users (user_id, channel) VALUES (?, ?)', (user_id, channel,))
+            conn.commit()
+            embed = discord.Embed(
+                title='Registration completed',
+                color=0x219ddd
+            )
+        else:
+            embed = discord.Embed(
+                title='Error!',
+                color=0xbf1e33
+            )
+            embed.add_field(name='Invalid channel parameter.', value='Please mention the channels that exist')
+
+    await interaction.response.send_message(embed=embed)
+
+# /unregister  prograssの登録を解除
+@tree.command(name='unregister', description='prograssの登録を解除')
+async def unregister(interaction: discord.Interaction):
+    user_id = interaction.user.id
+
+    if registered(user_id):
+        cursor.execute('DELETE FROM users WHERE user_id = ?', (user_id,))
+        conn.commit()
+        embed = discord.Embed(
+            title='Unregistration completed',
+            color=0x219ddd
+        )
+    else:
+        embed = discord.Embed(
+            title='Error!',
+            color=0xbf1e33
+        )
+        embed.add_field(name='You are not yet registered.', value='')
+
+    await interaction.response.send_message(embed=embed)
+
+# /submit  進捗を登録する
+@tree.command(name='submit', description='進捗を登録')
+@app_commands.describe(progress='進捗内容')
+async def submit(interaction: discord.Interaction, progress: str):
+    user_id = interaction.user.id
+
+    if registered(user_id):
+        cursor.execute('INSERT INTO progress (user_id, message) VALUES (?, ?)', (user_id, progress))
+        conn.commit()
+        embed = discord.Embed(
+            title='Progress was submitted',
+            color=0x219ddd
+        )
+        embed.add_field(name='Progress:', value=progress)
+    else:
+        embed = discord.Embed(
+            title='Error!',
+            color=0xbf1e33
+        )
+        embed.add_field(name='You were not registered.', value='Please register using /register')
+    await interaction.response.send_message(embed=embed)
+
+# /config  ユーザーごとの設定を変更
+@tree.command(name='config', description='設定を変更')
+@app_commands.describe(key='変更する項目 (channel, noticeのいずれか)', value='変更後の値 (channel -> チャンネルをメンション  notice -> HH:MM)')
+async def config(interaction: discord.Interaction, key: str, value: str):
+    if registered(interaction.user.id):
+        if key == 'channel':
+            channel, judge = channel_judge(value)
+            if judge:
+                cursor.execute('UPDATE users SET channel = ? WHERE user_id = ?', (channel, interaction.user.id))
+                conn.commit()
+                embed = discord.Embed(
+                    title='Channel config have been successfully updated',
+                    color=0x219ddd
+                )
+            else:
+                embed = discord.Embed(
+                    title='Error!',
+                    color=0xbf1e33
+                )
+                embed.add_field(name='Invalid channel parameter.', value='Please mention the channels that exist')
+        elif key == 'notice':
+            if not re.fullmatch('([01][0-9]|2[0-3]):[0-5][0-9]', value):
+                value = ''
+            cursor.execute('UPDATE users SET notice = ? WHERE user_id = ?', (value, interaction.user.id))
+            conn.commit()
+            embed = discord.Embed(
+                title='Notice config have been successfully updated',
+                color=0x219ddd
+            )
+            embed.add_field(name='New config:', value='No notification' if value == '' else value)
+        else:
+            embed = discord.Embed(
+                title='Error!',
+                color=0xbf1e33
+            )
+            embed.add_field(name='A key that does not exist.', value='Please specify a key that exists')
+    else:
+        embed = discord.Embed(
+            title='Error!',
+            color=0xbf1e33
+        )
+        embed.add_field(name='You were not registered.', value='Please register using /register')
+    await interaction.response.send_message(embed=embed)
+
+# /fubuki  こんこんきーつね!!
+@tree.command(name='fubuki', description='こんこんきーつね!!')
+async def fubuki(interaction: discord.Interaction):
+    embed = discord.Embed(
+        title='こんこんきーつね！(^・ω・^§)ﾉ',
+        color=0x53c7ea
+    )
+    await interaction.response.send_message('🌽'*32, embed=embed)
+
+# /aggregate  現時点での進捗一覧
+@tree.command(name='aggregate', description='今日00:00から現時点までの進捗一覧を表示')
+async def aggregate(interaction: discord.Interaction):
+    if registered(interaction.user.id):
+        progress = aggr_internal(interaction.user.id)
+
+        if progress == []:
+            embed = discord.Embed(
+                title='現時点での進捗はありません',
+                color=0xfa8000,
+            )
+        else:
+            embed = discord.Embed(
+                title='現時点での進捗一覧',
+                color=0x219ddd
+            )
+            embed.set_author(
+                name=interaction.user.name,
+                icon_url=interaction.user.avatar.url
+            )
+            embed.add_field(name='', value=''.join([f'1. {p[0]}\n' for p in progress]))
+    else:
+        embed = discord.Embed(
+            title='Error!',
+            color=0xbf1e33
+        )
+        embed.add_field(name='You were not registered.', value='Please register using /register')
+
+    await interaction.response.send_message(embed=embed)
+
+@tasks.loop(seconds=60)
+async def cron():
+    now = datetime.now().strftime('%H:%M')
+    # aggregateの定時処理・投票処理
+    if now == '00:00':
+        cursor.execute('SELECT user_id FROM users')
+        users = cursor.fetchall()
+
+        async def send_progress(user_id):
+            # 進捗送信
+            progress = aggr_internal(user_id)
+            user = await client.fetch_user(user_id)
+            embed = discord.Embed(
+                title='今日の進捗',
+                color=0xb9c42f
+            )
+            embed.set_author(
+                name=user.name,
+                icon_url=user.avatar.url
+            )
+            embed.add_field(name='', value=''.join([f'1. {p[0]}\n' for p in progress]))
+            text = f'<@{user_id}>'
+
+            cursor.execute('SELECT channel FROM users WHERE user_id = ?', (user_id,))
+            channel = client.get_channel(cursor.fetchone()[0])
+            msg = await channel.send(text, embed=embed)
+
+            # バックアップ・削除
+            cursor.execute('SELECT message, timestamp FROM progress WHERE user_id = ?', (user_id,))
+            for message, timestamp in cursor.fetchall():
+                backup_cursor.execute('INSERT INTO progress (user_id, message, timestamp) VALUES (?, ?, ?)', (user_id, message, timestamp))
+            cursor.execute('DELETE FROM progress WHERE user_id = ?', (user_id,))
+            backup_conn.commit()
+            conn.commit()
+
+            # 投票作成
+            await msg.add_reaction('👍')
+            await msg.add_reaction('👎')
+            await asyncio.sleep(86400)
+            # 投票集計
+            msg = await msg.channel.fetch_message(msg.id)
+            consent = discord.utils.get(msg.reactions, emoji='👍').count - 1
+            refusal = discord.utils.get(msg.reactions, emoji='👎').count - 1
+            cursor.execute('INSERT INTO votes (user_id, message_id, consent, refusal) VALUES (?, ?, ?, ?)', (user_id, msg.id, consent, refusal,))
+            conn.commit()
+
+        for u in users:
+            asyncio.create_task(send_progress(u[0]))
+
+    # 通知処理
+    async def notice(user_id):
+        cursor.execute('SELECT notice FROM users WHERE user_id = ?', (user_id,))
+        notice_result = cursor.fetchone()
+        if notice_result and now == notice_result[0]:
+            if aggr_internal(user_id) == []:
+                embed = discord.Embed(
+                    title='<:custom_emoji:1384184744878805027>発生中!!!!',
+                    color=0xb92946
+                )
+                user = await client.fetch_user(user_id)
+                embed.set_author(
+                    name=user.name,
+                    icon_url=user.avatar.url
+                )
+                embed.add_field(name='早く進捗出しやがれください！！', value='<:custom_emoji:1384184744878805027>'*16)
+                file = discord.File(fp='./shinchoku_nainatta.png', filename='shinchoku_nainatta.png', spoiler=False)
+                embed.set_image(url='attachment://shinchoku_nainatta.png')
+                text = f'<@{user_id}>\n' + '<:custom_emoji:1384184744878805027>'*12
+
+                cursor.execute('SELECT channel FROM users WHERE user_id = ?', (user_id,))
+                channel = client.get_channel(cursor.fetchone()[0])
+                await channel.send(text, file=file, embed=embed)
+
+    cursor.execute('SELECT user_id FROM users')
+    users = cursor.fetchall()
+    for u in users:
+        asyncio.create_task(notice(u[0]))
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
